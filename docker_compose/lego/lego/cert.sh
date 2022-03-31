e0dc1294292f070236ec356a0a0a182b68175070
Hailong Wang <whlsxl@gmail.com>
2022-04-01 07:07:49 +0800

    fix: [lego] dns resolv

--- docker_compose/lego/lego/cert.sh
+++ docker_compose/lego/lego/cert.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Setting DNS server to $LEGO_DNS_SERVER"
+echo "nameserver $LEGO_DNS_SERVER" > /etc/resolv.conf
 echo "Applying for a certificate"
 $( lego -a -m=$LEGO_EMAIL --domains $BASE_DOMAIN_NAME --domains *.$BASE_DOMAIN_NAME --dns $LEGO_DNS_PROVIDER --dns.resolvers $LEGO_DNS_SERVER --path /certs --pem renew --days 30 )
 RESULT=$?
