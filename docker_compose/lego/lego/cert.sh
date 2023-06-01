3cb5887d67afb8d453096968e40c0394bbab823c
Hailong Wang <whlsxl@gmail.com>
2023-06-02 03:27:50 +0800

    feat: lego update dns once

--- docker_compose/lego/lego/cert.sh
+++ docker_compose/lego/lego/cert.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-echo "Setting DNS server to $LEGO_DNS_SERVER"
-echo "nameserver $LEGO_DNS_SERVER" > /etc/resolv.conf
 echo "Applying for a certificate"
 $( /lego -a -m=$LEGO_EMAIL --domains $BASE_DOMAIN --domains *.$BASE_DOMAIN --path /certs --pem --dns $DNS_PROVIDER --dns.resolvers $LEGO_DNS_SERVER renew --days 30 )
-RESULT=$?
-if [ $RESULT != 0 ]; then
+if [ $? -ne 0 ]; then
   $( /lego -a -m=$LEGO_EMAIL --domains $BASE_DOMAIN --domains *.$BASE_DOMAIN --path /certs --pem --dns $DNS_PROVIDER --dns.resolvers $LEGO_DNS_SERVER run )
+  # echo "Fix certs permission..."
+  # chown lego:lego -R /certs/
 fi
--- docker_compose/lego/lego/run.sh
+++ docker_compose/lego/lego/run.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 echo "Run script"
+
+# id lego > /dev/null 2>&1
+# if [ $? -ne 0 ]; then
+#   echo "Create user lego($PUID):lego($PGID)"
+#   addgroup lego -g $PGID -S
+#   adduser -G lego lego -u $PUID -D -H -g lego -h /
+# fi
+
+echo "Setting DNS server to $LEGO_DNS_SERVER"
+echo "nameserver $LEGO_DNS_SERVER" > /etc/resolv.conf
+
 /root/cert.sh
+
 echo "Run cron"
 exec crond -l 2 -f
